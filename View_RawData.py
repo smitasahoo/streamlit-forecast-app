195d2aa6aa607307091f99e3f37efaa1cc742930
smitasahoo <sahoo.smita880@gmail.com>
2022-07-04 10:48:03 -0400

    Add files via upload

---
+++ View_RawData.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('View Raw Data')
+#Get data_load_state
+
+import snowflake.connector
+from snowflake.connector import ProgrammingError
+
+connect=snowflake.connector.connect(user='SMITASAHOO',password='Smita@94',account='JY60240.ca-central-1.aws')#,region='ca-central-1',cloud='aws')
+                                    #database='SNOWFLAKE_SAMPLE_DATA',schema='INFORMATION_SCHEMA')
+#connect=snowflake.connector.connect(user='SMITA.SAHOO@AGROPUR.COM',password='Sm16sa2022',account='bk48470.us-east-2.aws')#,region='us-east-2',cloud='aws')
+                                    
+cx=connect.cursor()
+#cx.execute('select * from forecast')
+sql='select * from TEST.TEST_FORECAST.FORECAST'
+def fetch_pandas_old(cur, sql):
+    cur.execute(sql)
+    rows = 0
+    while True:
+        dat = cur.fetchmany(50000)
+        if not dat:
+            break
+        df = pd.DataFrame(dat,columns=['XXAGR_PUZ_PROD_PROMOTION_GRP','F_PUZ_ATTR_FAMBAN_BANN_GRP','XXAGR_PUZ_REGION_GROUP','WEEK_START_DATE','NET_INVOICED_QTY_MV','Forecast','Update_Forcast'])
+        rows += df.shape[0]
+    return df
+data_load_state = st.text('Loading data...')
+data = fetch_pandas_old(cx,sql)
+data = data.astype({'NET_INVOICED_QTY_MV':'float','Forecast':'float','Update_Forcast':'float'})
+data_load_state.text('Loading data...done!')
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+@st.cache
+def convert_df(df):
+  return df.to_csv().encode('utf-8')
+
+csv = convert_df(data)
+
+st.download_button(
+     label="Download data as CSV",
+     data=csv,
+     file_name='df.csv',
+     mime='text/csv',
+ )
